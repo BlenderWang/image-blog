0d6ec58078bbff08a21e3b32e37b1e54b1eb360c
shirley_wang <shirleywang527@gmail.com>
2022-04-17 08:44:34 +0200

    scroll to top button

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,13 @@
 import Nav from "./Nav";
 import Footer from "./Footer";
+import ScrollToTopButton from "./ScrollToTopButton";
 
 const Layout = ({ children, categories, seo }) => (
     <>
         <Nav categories={categories} />
         {children}
         <Footer />
+        <ScrollToTopButton />
     </>
 );
 
