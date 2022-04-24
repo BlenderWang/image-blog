e71fa07736099c305fdf91753d1bbd75b07e4a3f
shirley_wang <shirleywang527@gmail.com>
2022-04-24 13:24:21 +0200

    modify next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         loader: "default",
-        domains: ["res.cloudinary.com", "localhost"],
+        domains: ["res.cloudinary.com"],
     },
 };
 
