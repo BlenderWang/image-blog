d1238a79e50c809beca0bdc15aecf6b3ac1f170d
shirley <shirleywang527@gmail.com>
2022-09-14 14:08:10 +0200

    style fix

--- components/Card.js
+++ components/Card.js
@@ -3,29 +3,29 @@ import Link from "next/link";
 import NextImage from "./Image";
 
 const Card = ({ article }) => {
-    return (
-        <Link href={`/article/${article.attributes.slug}`}>
-            <a className="hover:underline-offset-4 transition ease-in-out duration-300">
-                <div className="break-inside-avoid relative transform opacity-100">
-                    <div className="">
-                        <NextImage image={article.attributes.image} />
-                    </div>
+	return (
+		<div className="break-inside-avoid relative transform opacity-100">
+			<Link href={`/article/${article.attributes.slug}`}>
+				<a className="hover:underline-offset-4 transition ease-in-out duration-300">
+					<div className="">
+						<NextImage image={article.attributes.image} />
+					</div>
 
-                    <div className="invisible lg:visible absolute inset-0 flex flex-col justify-center items-center transition duration-200 ease-in opacity-0 hover:opacity-100">
-                        <div className="absolute inset-0 bg-slate-900 opacity-50"></div>
+					<div className="invisible lg:visible absolute inset-0 flex flex-col justify-center items-center transition duration-200 ease-in opacity-0 hover:opacity-100">
+						<div className="absolute inset-0 bg-slate-900 opacity-50"></div>
 
-                        <p id="category" className="text-xl text-yellow-100">
-                            {article.attributes.category.data.attributes.name}
-                        </p>
+						<p id="category" className="text-xl text-yellow-100">
+							{article.attributes.category.data.attributes.name}
+						</p>
 
-                        <p id="title" className="text-3xl text-white">
-                            {article.attributes.title}
-                        </p>
-                    </div>
-                </div>
-            </a>
-        </Link>
-    );
+						<p id="title" className="text-3xl text-white">
+							{article.attributes.title}
+						</p>
+					</div>
+				</a>
+			</Link>
+		</div>
+	);
 };
 
 export default Card;
--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        loader: "default",
-        domains: ["res.cloudinary.com"],
-    },
+	reactStrictMode: true,
+	images: {
+		loader: "default",
+		domains: ["res.cloudinary.com", "localhost"],
+	},
 };
 
 module.exports = nextConfig;
